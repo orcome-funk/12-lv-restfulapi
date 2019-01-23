af6aefd63ba13c2cc96ee2832f651bc1a313a094
Akhmad Herdian <akhmad.herdian@gmail.com>
2019-01-24 06:52:39 +0800

    Make store validation in MeetingController

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -24,6 +24,13 @@ class MeetingController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'title'       => 'required',
+            'description' => 'required',
+            'time'        => 'required',
+            'user_id'     => 'required',
+        ]);
+
         $title = $request->input('title');
         $description = $request->input('description');
         $time = $request->input('time');
