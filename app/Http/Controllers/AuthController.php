51f0c3b897405024c7ac99d4c8414fb52fd852a4
Akhmad Herdian <akhmad.herdian@gmail.com>
2019-01-23 21:43:25 +0800

    Add store and signin function in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,5 +4,25 @@ namespace App\Http\Controllers;
 
 class AuthController extends Controller
 {
-    //
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Signin a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function signin(Request $request)
+    {
+        //
+    }
 }
