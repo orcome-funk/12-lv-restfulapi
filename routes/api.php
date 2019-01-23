b3fe4f088059cf2ef7242db1d02bbf055841e13d
Akhmad Herdian <akhmad.herdian@gmail.com>
2019-01-23 21:27:07 +0800

    Make route meeting and user

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +11,23 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['prefix' => 'v1'], function () {
+
+    Route::resource('meeting', 'MeetingController', [
+        'except' => ['create', 'edit'],
+    ]);
+    Route::resource('meeting/registration', 'RegisterController', [
+        'only' => ['store', 'destroy'],
+    ]);
+    Route::post('/user/register', [
+        'uses' => 'AuthController@store',
+    ]);
+    Route::post('/user/signin', [
+        'uses' => 'AuthController@signin',
+    ]);
+
 });
