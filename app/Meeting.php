aaee3f5c57db48333fd4741391fd786b27dbeeef
Akhmad Herdian <akhmad.herdian@gmail.com>
2019-01-24 11:30:22 +0800

    Make relationships between users and meetings table

--- app/Meeting.php
+++ app/Meeting.php
@@ -2,9 +2,15 @@
 
 namespace App;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Meeting extends Model
 {
-    //
+    protected $fillable = ['title', 'description', 'time'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -2,8 +2,8 @@
 
 namespace App;
 
+use App\Meeting;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
@@ -27,4 +27,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function meetings()
+    {
+        return $this->belongsToMany(Meeting::class);
+    }
 }
