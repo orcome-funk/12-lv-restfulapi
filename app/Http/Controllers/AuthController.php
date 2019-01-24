06ac2a16d4ff5bd46bc5ca45919bac21f6f09a73
Akhmad Herdian <akhmad.herdian@gmail.com>
2019-01-24 11:35:13 +0800

    Make user data input at the function store in the AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use Illuminate\Http\Request;
+
 class AuthController extends Controller
 {
     /**
@@ -12,8 +15,38 @@ class AuthController extends Controller
      */
     public function store(Request $request)
     {
-        return 'It works';
-    }
+        $this->validate($request, [
+            'name'     => 'required',
+            'email'    => 'required|email',
+            'password' => 'required|min:5',
+        ]);
+
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $user = new User([
+            'name'     => $name,
+            'email'    => $email,
+            'password' => bcrypt('$password'),
+        ]);
+
+        if ($user->save()) {
+            $user->signin = [
+                'href'   => 'api/v1/user/signin',
+                'method' => 'POST',
+                'params' => 'email, password',
+            ];
+            $response = [
+                'msg'  => 'User created',
+                'user' => $user,
+            ];
+            return response()->json($response, 201);
+        }
+        $response = [
+            'msg' => 'An error occurred',
+        ];
+        return response()->json($response, 404);}
 
     /**
      * Signin a newly created resource in storage.
