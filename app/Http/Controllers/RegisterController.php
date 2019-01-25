7270747323fe20cd503cb93344b1a0159f376576
Akhmad Herdian <akhmad.herdian@gmail.com>
2019-01-25 11:14:15 +0800

    Make store and destroy function in RegisterController

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use App\Meeting;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -14,7 +16,41 @@ class RegisterController extends Controller
      */
     public function store(Request $request)
     {
-        return 'It works';
+        $this->validate($request, [
+            'meeting_id' => 'required',
+            'user_id'    => 'required',
+        ]);
+        $meeting_id = $request->input('meeting_id');
+        $user_id = $request->input('user_id');
+
+        $meeting = Meeting::findOrFail($meeting_id);
+        $user = User::findOrFail($user_id);
+
+        $message = [
+            'msg'        => 'User is already registered for meeting',
+            'user'       => $user,
+            'meeting'    => $meeting,
+            'unregister' => [
+                'href'   => 'api/v1/meeting/registration/'.$meeting->id,
+                'method' => 'DELETE',
+            ],
+        ];
+        if ($meeting->users()->where('users.id', $user->id)->first()) {
+            return response()->json($message, 404);
+        }
+        $user->meetings()->attach($meeting);
+
+        $response = [
+            'msg'        => 'User registered for meeting',
+            'user'       => $user,
+            'meeting'    => $meeting,
+            'unregister' => [
+                'href'   => 'api/v1/meeting/registration/'.$meeting->id,
+                'method' => 'DELETE',
+            ],
+        ];
+        return response()->json($response, 201);
+
     }
 
     /**
@@ -25,6 +61,19 @@ class RegisterController extends Controller
      */
     public function destroy($id)
     {
-        return 'It works';
+        $meeting = Meeting::findOrFail($id);
+        $meeting->users()->detach();
+
+        $response = [
+            'msg'      => 'User unregistered for meeting',
+            'user'     => 'tbd',
+            'meeting'  => $meeting,
+            'register' => [
+                'href'   => 'api/v1/meeting/registration',
+                'method' => 'POST',
+                'params' => 'user_id, meeting_id',
+            ],
+        ];
+        return response()->json($response, 200);
     }
 }
